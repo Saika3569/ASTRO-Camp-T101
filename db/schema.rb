149a82e8aff831b5499bbeea020b2200e5766e44
Saika <kyon4501@gmail.com>
2021-01-31 16:36:39 +0800

    fix model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_074552) do
+ActiveRecord::Schema.define(version: 2021_01_31_082617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,8 @@ ActiveRecord::Schema.define(version: 2021_01_31_074552) do
     t.string "title"
     t.string "content"
     t.bigint "user_id", null: false
-    t.string "tag"
     t.string "priority"
-    t.string "aasm_state"
+    t.string "state"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
