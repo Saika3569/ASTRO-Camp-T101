72c098c85126d0bed40106907893788cda869a09
Saika <kyon4501@gmail.com>
2021-02-19 18:20:59 +0800

    topic 8 first fix

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -9,12 +9,11 @@ RSpec.describe TasksController, type: :controller do
   end
 
   describe "#show" do
-    before do
-      @task = create(:task)
-    end
+    
+    let(:task){ create(:task) }
 
     it "returns a 200 response" do
-      get :show, params: {id: @task.id }
+      get :show, params: {id: task }
       expect(response).to have_http_status "200"
     end
   end
@@ -40,26 +39,23 @@ RSpec.describe TasksController, type: :controller do
   end
 
   describe "#update" do
-    before do
-      @task = create(:task)
-    end
+    
+    let(:task){ create(:task) }
 
     it "edit a task" do
       task_params = attributes_for(:task,title: 'asdf')
-      patch :update, params: {id: @task.id, task: task_params }
-      expect(@task.reload.title).to eq "asdf"
+      patch :update, params: {id: task, task: task_params }
+      expect(task.reload.title).to eq "asdf"
     end
   end
 
   describe "#destroy" do
-    before do
-      @task = create(:task)
-    end
+    
+    let!(:task){ create(:task) }
 
     it "destoy a task" do
-      task_params = attributes_for(:task,title: 'asdf')
       expect{
-        delete :destroy, params: {id: @task.id}
+        delete :destroy, params: {id: task }
       }.to change(Task, :count).by -1
     end
   end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,32 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it "is valid with a title, content, and user_id" do
-    task = build(:task)
-    expect(task).to be_valid
-  end 
-  it "is invalid without a title" do
-    task = build(:task,title: nil)
-    task.valid?
-    expect(task.errors[:title]).to include("can't be blank")
+  describe 'associations' do
+    it { should belong_to(:user).class_name('User') }
   end
-  it "is invalid without a content" do
-    task = build(:task,content: nil)
-    task.valid?
-    expect(task.errors[:content]).to include("can't be blank")
+
+  describe 'validations' do
+    subject { build(:task) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:content) }
+    it { should validate_uniqueness_of(:title).scoped_to(:user_id) }
   end
-  it "does not allow duplicate title per user" do
-    user = create(:user)
-    task = create(:task,user: user)
-    new_task = build(:task, user: user)
-    new_task.valid?
-    expect(new_task.errors[:title]).to include("has already been taken")
-  end
-  it "allows two users to share a project name" do
-    user = create(:user)
-    user_task = create(:task,user: user)
-    other_user = create(:other_user)
-    other_task = build(:task,user: other_user)
-    expect(other_task).to be_valid
-  end
+  
 end
