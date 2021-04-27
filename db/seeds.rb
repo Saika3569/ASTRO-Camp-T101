0155c71b66d84f86e25bfb81f8e9c7d631d4d796
Saika <kyon4501@gmail.com>
2021-04-28 04:26:03 +0800

    fix nodesass

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: 'Saika', account: 'saika123456', password: 'saikajlsdfj')
+User.create!(name: '管理員', email: 'zxcv@gmail.com', password: '123456', admin: true)
 
